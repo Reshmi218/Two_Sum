9d80385e9a03fa04dab28e76efd9c7013d35f62e
Reshmi218 <132146495+Reshmi218@users.noreply.github.com>
2023-08-14 16:08:27 +0530

    Add files via upload

---
+++ Reshmi.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Reshmi{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Running Reshmi...");
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int sum = n+m;
+        System.out.println("sum ="+ sum);
+    }
+}
